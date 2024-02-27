242a9b7c3b259f79e7b007bca0274cc7a2f624ab
nircniv <ng.ta.wei@outlook.com>
2024-02-27 21:44:45 +0800

    Fixed bug with toggling

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from src.globals import logger
 from src.app import zoom_service, player
 
 def main():
-     if "toggle" in sys.argv:
+    if "toggle" in sys.argv:
+        logger.log("Start Toggle.")
         zoom_service.toggle_zoom()
+        logger.log("End Toggle.")
+    else:
+        logger.log("Starting service.")
+        while not player.monitor.abortRequested():
+            if player.monitor.waitForAbort(10):
+                break
+        logger.log("Ending service.")
 
 if __name__ == "__main__":
     main()
-
-    while not player.monitor.abortRequested():
-        if player.monitor.waitForAbort(10):
-            break
-    logger.log("Ending service.")
